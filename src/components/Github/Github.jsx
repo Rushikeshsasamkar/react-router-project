b0fa12de49e60e3accf489130def1f49080aeae6
Rushikeshsasamkar <44942941+Rushikeshsasamkar@users.noreply.github.com>
2024-01-14 22:53:01 +0530

    styling

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import {useLoaderData} from 'react-router-dom'
+import { useLoaderData } from 'react-router-dom'
 
 const Github = () => {
 
@@ -16,12 +16,26 @@ const Github = () => {
     //     )
 
     // },[]);
+    console.log(data);
 
 
     return (
-        <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github Followers: {data.following}
+        <div className='m-12 rounded-lg shadow-2xl bg-slate-400 text-black font-serif p-4 text-3xl'>
+            <h1 className='text-center'>Github Details</h1>
+            <div className="flex flex-col md:flex-row items-start">
+                <img className=" rounded-lg mx-3 mb-3 md:mb-0" src={data.avatar_url} alt="Git Picture" width={300} />
+                <div className="grid gap-2 ml-0 md:ml-10">
+                    <label className="text-lg font-semibold">Name:</label>
+                    <label>{data.name}</label>
+                    <label className="text-lg font-semibold">Location:</label>
+                    <label>{data.location}</label>
+                    <label className="text-lg font-semibold">ID:</label>
+                    <label>{data.id}</label>
+                    <label className="text-lg font-semibold">Public Repositories:</label>
+                    <label>{data.public_repos}</label>
+                </div>
+            </div>
 
-            <img className='border-r-amber-300 rounded-lg' src={data.avatar_url} alt="Git Picture" width={300} />
         </div>
     )
 }
